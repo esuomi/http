43c64f4288f7d4ef682fb698401a96a53fb5d8fc
Esko Suomi <suomi.esko@gmail.com>
2015-03-01 21:56:47 +0200

    bit more documentation for HaltedException

--- http-core/src/main/java/io/induct/util/concurrent/HaltedException.java
+++ http-core/src/main/java/io/induct/util/concurrent/HaltedException.java
@@ -4,9 +4,25 @@ package io.induct.util.concurrent;
  * Thrown whenever running process, action or any equivalent is interrupted, cancelled or otherwise prevented from
  * finishing successfully.
  *
+ * This exception is similar to {@link java.util.concurrent.CancellationException} but provides exception chaining
+ * constructors.
+ *
  * @since 1.3.2015
  */
 public class HaltedException extends RuntimeException {
+
+    public HaltedException() {
+        super();
+    }
+
+    public HaltedException(String message) {
+        super(message);
+    }
+
+    public HaltedException(Throwable cause) {
+        super(cause);
+    }
+
     public HaltedException(String message, Throwable cause) {
         super(message, cause);
     }
